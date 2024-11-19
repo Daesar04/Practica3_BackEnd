404294d6d959f9de05db33666bbde0ec068574eb
Daesar04 <describanoa@alumnos.nebrija.es>
2024-11-19 12:02:04 +0100

    Initialize Deno project with MongoDB integration and basic server setup

---
+++ main.ts
@@ -0,0 +1,62 @@
+import { MongoClient } from "mongodb";
+import {  } from "./types.ts";
+import {  } from "./resolvers.ts";
+
+const url = Deno.env.get("MONGO_URL");
+
+if(!url) {
+  console.log("No se ha podido conectar a la URL");
+  Deno.exit(-1);
+}
+
+const client = new MongoClient(url);
+const dbName = 'nebrijadb';
+
+await client.connect();
+console.log('Connected successfully to server');
+const db = client.db(dbName);
+const nombreCollection = db.collection<>('');
+const nombreCollection = db.collection<>('');
+
+const handler = async (
+  req: Request
+): Promise<Response> => {
+  const method = req.method;
+  const url = new URL(req.url);
+  const path = url.pathname;
+
+  if(method === "GET")
+  {
+    if(path === "/ninos/buenos")
+    {
+      
+    }
+    else if(path === "/ninos/malos")
+    { 
+      
+    }
+    else if(path === "/entregas")
+    {
+
+    }
+    else if(path === "/ruta")
+    {
+
+    }
+  } 
+  else if(method === "POST")
+  {
+    if(path === "/ubicacion")
+    {
+      
+    }
+    else if(path === "/ninos")
+    { 
+      
+    }
+  } 
+
+  return new Response("Endpoint not found", { status: 400 });
+};
+
+Deno.serve({port: 6768}, handler);
